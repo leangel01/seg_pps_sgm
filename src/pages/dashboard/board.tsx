7c7566f71dbd22522caa8f81f3c109ee9e3bdb71
José Angel López Mendoza <jose.lopez@sfp.gob.mx>
2026-06-08 17:24:00 -0600

    chart cards added

--- src/pages/dashboard/board.tsx
+++ src/pages/dashboard/board.tsx
@@ -7,12 +7,10 @@ export const BoardView: React.FC = () => {
 
     return (
         <div style={{padding: "24px", minHeight: "100vh"}}>
+            <Space direction="vertical" size="middle" style={{ width: "100%" }}>
             <Affix offsetTop={64}>
                 <Card size="small" style={{borderRadius: "8px", borderTop: "4px solid #9b2247", boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)"}}>
                     <Row gutter={16} align="middle">
-                        <Col xs={24} md={6}>
-                            <Title level={4} style={{margin: 0}}>Tablero de Seguimiento</Title>
-                        </Col>
                         {/*SELECTOR DE AÑO*/}
                         <Col xs={24} md={6}>
                             <Select placeholder="Seleccionar año" style={{width: "100%"}}>
@@ -23,7 +21,29 @@ export const BoardView: React.FC = () => {
                     </Row>
                 </Card>
             </Affix>
-            En esta seccion se mostrara el tablero de seguimiento de presupuesto.
+            <Row>
+                <Col span={24}>
+                    <Card
+                    title ={"Ejercicio histórico del gasto"}
+                    style = {{ borderRadius: '8px', boxShadow: '0 2px 8px rgba(0,0,0,0.05)' }}
+                    > 
+                    SECCION GRAFICO DE LINEAS
+                    </Card>
+                </Col>
+            </Row>
+            {/* SECCION DE GRAFICO POR PROGRAMA*/}
+            <Row >
+                <Col span={24}>
+                    <Card
+                    title ={"Gasto por programa presupuestario"}
+                    style = {{ borderRadius: '8px', boxShadow: '0 2px 8px rgba(0,0,0,0.05)' }}
+                    >
+                    Seccion de grafico por programa presupuestario
+                    </Card>
+                </Col>
+            </Row>
+
+            </Space>   
         </div>
     );
 };
